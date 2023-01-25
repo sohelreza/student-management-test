ef4810912135ec5d98be1828709ede1f7b175ae5
sohelreza <61862128+sohelreza@users.noreply.github.com>
2023-01-26 03:16:15 +0600

    first validation going

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use App\Http\Requests\UpdateStudentRequest;
+use Validator;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        echo "index controller";
+        die();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        echo "controller create";
+        die();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreStudentRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'class' => 'required | max:11',
+            'name' => 'required | max:100',
+            'email' => 'required | max:100 | email:rfc,dns',
+            'contact_number' => 'required | numeric | digits:11',
+            'status' => 'required | in:unconfirmed,admitted,terminated',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['success' => false,'error' => 'make sure to fill up all the fields'], 401);
+        }
+
+        dd($validator);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateStudentRequest  $request
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateStudentRequest $request, Student $student)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_01_22_143514_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('class');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('contact_number')->unique();
+            $table->enum('status', ['unconfirmed', 'admitted','terminated']);
+            $table->string('field_ids');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
